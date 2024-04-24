a2c82f78648b27a20bd1189b75813417e5d7a16a
Morten Lied Johansen <mortenjo@ifi.uio.no>
2024-04-24 12:42:08 +0200

    Add some command aliases if installed

--- files/bash-aliases.sh
+++ files/bash-aliases.sh
@@ -6,3 +6,21 @@ fi
 
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
+
+# New and improved tools
+function replace() {
+  if command -v "${2}" >/dev/null; then
+    # shellcheck disable=SC2139
+    alias "${1}=${2}"
+  fi
+}
+
+replace ls eza
+replace ls exa
+replace cat bat
+replace cat batcat
+replace du dust
+replace df duf
+replace ps px
+
+unset replace
